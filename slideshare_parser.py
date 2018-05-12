08a9ce813c4037fb540099d6b20bc0980dfb62c4
Geeko_forensic <pveutin@users.noreply.github.com>
2018-05-12 22:43:23 +0200

    mise à jour slideshare_parser.py
    ajout de la prise en charge d'argument (url) et correction disposition de l'image dans le PDF

--- slideshare_parser.py
+++ slideshare_parser.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# coding : utf-8
 __author__ = "Rooxy"
 __copyright__ = "Copyright 2018, Rooxy"
 __license__ = "GPL v3"
@@ -11,6 +12,7 @@ import requests
 import json
 import urllib
 import os
+import argparse
 
 from bs4 import BeautifulSoup
 from reportlab.pdfgen import canvas
@@ -18,20 +20,19 @@ from reportlab.lib.units import inch, cm
 from reportlab.lib.pagesizes import landscape, letter
 from reportlab.platypus import Image
 
-url = "https://fr.slideshare.net/ctin/windows-7-forensics-overviewr3"
-
+parser = argparse.ArgumentParser(description='Récupère les dispos de slideshare')
+parser.add_argument('-u', '--url', help='url du diaporama a récupérer', required=True, dest="url")
+results = parser.parse_args()
+url = results.url
 resultat = requests.get(url)
 page = resultat.content
 soup = BeautifulSoup(page,  "html.parser")
 link = soup.find('link', {"title":"Slideshow json oEmbed Profile"})
 url_json = link['href']
-
 structure = requests.get(url_json)
 j = json.loads(structure.content) 
 base_url = "https:"+j['slide_image_baseurl']
-
 slide_count = j['total_slides']
-
 
 try: 
     os.makedirs(j['title'])
@@ -43,8 +44,8 @@ c.setPageSize( landscape(letter) )
 width,height = landscape(letter)
 
 for i in range(slide_count):
-    uri = base_url+str(i+1)+j['slide_image_baseurl_suffix']
-    urllib.urlretrieve (uri, './'+j['title']+'/'+str(i+1)+j['slide_image_baseurl_suffix'])
-    c.drawImage('./'+j['title']+'/'+str(i+1)+j['slide_image_baseurl_suffix'], 1*inch, 1*inch, width-inch, height-inch)
-    c.showPage()
+	uri = base_url+str(i+1)+j['slide_image_baseurl_suffix']
+	urllib.urlretrieve (uri, './'+j['title']+'/'+str(i+1)+j['slide_image_baseurl_suffix'])
+	c.drawImage('./'+j['title']+'/'+str(i+1)+j['slide_image_baseurl_suffix'], 1*inch, 1*inch, width-2*inch, height-2*inch)
+	c.showPage()
 c.save()
